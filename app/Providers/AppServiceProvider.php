b42b7c77d7309586adab8b661e5f250852943e21
Javi0300 <93288606+Javi0300@users.noreply.github.com>
2024-01-15 13:09:20 -0600

    Update AppServiceProvider.php
    jijijij

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,48 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()/* : void */
     {
-        //$mindex= substr(isset($_SERVER['REQUEST_URI']),(strlen(isset($_SERVER['REQUEST_URI'])))-9,9); 
+        public function boot()/* : void */
+    {
+        $mDir= ($_SERVER["REQUEST_URI"]); //Poner isset de ser necesario para que funcione el config:clear y el artisan serve y luego quitarle isset para que reciba url desde el navegador
+        //$mDir= isset($_SERVER["REQUEST_URI"]);
+        $mruta=explode("/", $mDir );         
+        try {
+            $mFile =public_path() .'/' .$mruta[1].'/'.$mruta[2]."/confbd.txt";
+        }catch (Exception $e){
+            $mFile=public_path()."/laboratorios/inadware/confbd.txt";
+
+        }
+        $xml = simplexml_load_file($mFile);
+        /* $appkey = implode(" ",explode(":",$xml->appkey)); */
+        $url_dinamica = $xml->url_dinamica;
+        $mtxthost =implode(" ",explode(":",$xml->servidor));
+        $mtxtport =implode(" ",explode(":",$xml->puerto));
+        $laboratorio =implode(" ",explode(":",$xml->laboratorio));
+        $logotipo = implode(" ",explode(":",$xml->logotipo));
+        $mtxtdatabase =implode(" ",explode(":",$xml->bd));
+        $mtxtusername =implode(" ",explode(":",$xml->usuariobd));
+        $mtxtpassword =implode(" ",explode(":",$xml->pwdbd));
         
+        view()->composer('*',function($view) use($laboratorio,$logotipo,$url_dinamica){
+            $view->with('laboratorio', $laboratorio);
+            $view->with('logotipo', $logotipo);
+            //$view->with('appkey', $appkey);
+            $view->with('url_dinamica', $url_dinamica);
+        });
+        //dump(session("nombre_laboratorio"));
+        Config::set('database.connections.mysql', [
+            'driver' => 'mysql',
+            'host' => $mtxthost,//$xml->servidor,
+            'port' => $mtxtport,//$mailsetting->puerto_sql,
+            'database' => $mtxtdatabase,//$xml->parametros->bd,//$mailsetting->base_de_datos,
+            'username' =>  $mtxtusername,//$mailsetting->usuario_sql,
+            'password' => $mtxtpassword,//$mailsetting->password_sql,
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ]);
+    }
     }
 }
